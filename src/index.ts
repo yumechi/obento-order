b82d812b7a5944ea66e3392bd269c1e7ffa9031f
yumechi <yumechi0525@gmail.com>
2019-08-24 15:08:50 +0900

    コマンドライン引数を使ってみる

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,39 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
+const commandLineArgs = require("command-line-args");
+
+function getArgs(): { [key: string]: boolean} {
+  const optionDefinitions = [
+    {
+      name: "dotenv",
+      alias: "d",
+      type: Boolean
+    },
+    {
+      name: "puppeteer",
+      alias: "p",
+      type: Boolean
+    },
+  ];
+  return commandLineArgs(optionDefinitions);
 }
 
-console.log(hello("World"));
+function callDotenvTest(): void {
+  // mock
+  console.log("dotenv called");
+}
+
+function callPuppeteerTest(): void {
+  // mock
+  console.log("Puppeteer called");
+}
+
+function main(): void {
+  const options: {[key: string]: boolean} = getArgs();
+  if(options["dotenv"]) {
+    callDotenvTest();
+  } if(options["puppeteer"]) {
+    callPuppeteerTest();
+  }
+  console.log(options);
+}
+
+main();
